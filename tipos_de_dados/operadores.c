3731a61fc2a46b44b568d6adffb29996ede313a9
JoaoHSantana2007 <jhfcsant2007@gmail.com>
2025-03-28 12:42:59 -0300

    a

--- tipos_de_dados/operadores.c
+++ tipos_de_dados/operadores.c
@@ -8,51 +8,62 @@ int main(int argc, char* argv[]){
 
     //n1 = atof(argv[1]);
     //n2 = atof(argv[2]);
-    printf("Digite o valor de n1: ");
-    scanf("%f", &n1);
+        
+        printf("Digite o valor de n1: ");
+        scanf("%f", &n1);
 
-    printf("Digite o valor de n2: ");
-    scanf("%f", &n2);
+        printf("Digite o valor de n2: ");
+        scanf("%f", &n2);
 
     // Adição
-    float soma = n1 + n2;
-    printf("%.2f + %.2f = %.2f\n", n1, n2, soma);
+        
+        float soma = n1 + n2;
+        printf("%.2f + %.2f = %.2f\n", n1, n2, soma);
 
     // Subtração
-    float diferenca = n1 - n2;
-    printf("%.2f - %.2f = %.2f\n", n1, n2, diferenca);
+        
+        float diferenca = n1 - n2;
+        printf("%.2f - %.2f = %.2f\n", n1, n2, diferenca);
 
     // Multiplicação
-    float produto = n1 * n2;
-    printf("%.2f * %.2f = %.2f\n", n1, n2, produto);
+        
+        float produto = n1 * n2;
+        printf("%.2f * %.2f = %.2f\n", n1, n2, produto);
 
     // Divisão
-    float quociente = n1 / n2;
-    printf("%.2f / %.2f = %.2f\n", n1, n2, quociente);
+        
+        float quociente = n1 / n2;
+        printf("%.2f / %.2f = %.2f\n", n1, n2, quociente);
 
     // Potenciação
-    float resultado_potenciacao = powf(n1, n2);
-    printf("%.2f^%.2f = %.2f\n", n1, n2, resultado_potenciacao);
+        
+        float resultado_potenciacao = powf(n1, n2);
+        printf("%.2f^%.2f = %.2f\n", n1, n2, resultado_potenciacao);
 
     // Radiciação
-    float raiz_quadrada = sqrtf(n1);
-    printf("%.2f^1/2 = %.2f\n", n1, raiz_quadrada);
+        
+        float raiz_quadrada = sqrtf(n1);
+        printf("%.2f^1/2 = %.2f\n", n1, raiz_quadrada);
 
     // Resto da divisão
-    int resto = (int) n1 % (int) n2;
-    printf("%.0f %% %.0f = %d\n", n1, n2, resto);
+    
+        int resto = (int) n1 % (int) n2;
+        printf("%.0f %% %.0f = %d\n", n1, n2, resto);
 
     // Quociente inteiro da divisão
-    int quociente_inteiro = n1 / (int) n2;
-    printf("%.2f // %.2f = %d\n", n1, n2, quociente_inteiro);
+       
+        int quociente_inteiro = n1 / (int) n2;
+        qprintf("%.2f // %.2f = %d\n", n1, n2, quociente_inteiro);
 
     // Incremento
-    n1++;
-    printf("%.2f++ = %.2f\n", n1-1, n1);
+        
+        n1++;
+        printf("%.2f++ = %.2f\n", n1-1, n1);
 
     // Decremento
-    n1--;
-    printf("%.2f-- = %.2f\n", n1+1, n1);
+       
+        n1--;
+        printf("%.2f-- = %.2f\n", n1+1, n1);
 
     return 0;
 }
