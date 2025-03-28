22287e1912220a2d0910f390e32c84ee20693194
JoaoHSantana2007 <jhfcsant2007@gmail.com>
2025-03-28 12:24:08 -0300

    a

--- tipos_de_dados/numericos.c
+++ tipos_de_dados/numericos.c
@@ -4,25 +4,37 @@
 
 int main(int argc, char* argv[]){
     
+    //Digite a idade
+        
         int idade;
+
+            printf("digite a idade: ");
+            scanf("%d", &idade);
+
+    //Digite a altura     
+        
         float altura;
+
+            printf("Digite a altura: ");
+            scanf("%f", &altura);
+
+    //Digite o peso
+        
         float peso;
+        
+            printf("Digite o peso: ");
+            scanf("%f", &peso);
     
-        printf("digite a idade: ");
-        scanf("%d", &idade);
-   
-        printf("Digite a altura: ");
-        scanf("%f", &altura);
+    //Coputador entende a idade,altura e peso
+        
+            printf("A idade eh %d\n" , idade );
+            printf("a altura eh %.2f\n", altura);
+            printf("O peso eh %.2f\n", peso);
 
-        printf("Digite o peso: ");
-        scanf("%f", &peso);
-    
-        printf("A idade eh %d\n" , idade );
-        printf("a altura eh %.2f\n", altura);
-        printf("O peso eh %.2f\n", peso);
+    //Calculo dobro de peso
 
-        float dobro_peso = peso * 2.0;
-        printf("O dobro do peso eh %.2f", dobro_peso);
+            float dobro_peso = peso * 2.0;
+            printf("O dobro do peso eh %.2f", dobro_peso);
  
     return 0;
     
