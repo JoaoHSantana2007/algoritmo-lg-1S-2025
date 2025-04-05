4b8e4060a50ff030602c227dc65ec31cc3a4a4fb
JoaoHSantana2007 <jhfcsant2007@gmail.com>
2025-04-04 22:47:32 -0300

    a

--- atividade2/area.c
+++ atividade2/area.c
@@ -2,53 +2,74 @@
 #include <math.h>
 
 int main(int argc, char* argv[]){
-
+    
     char opcao;
     float r, D, d, L, b, h, B, A;
 
-        printf("area.c\n");
-        printf("escolha uma opçao: \n");
-        printf("c - circulo\n");
-        printf("l - losango\n");
-        printf("q - quadrado\n");
-        printf("r - retangulo\n");
-        printf("z - trapezio\n");
-        printf("t - triangulo\n");
+    printf("area.c\n");
+    printf("Escolha uma opcao: \n");
+    printf("c - Circulo\n");
+    printf("l - Losango\n");
+    printf("q - Quadrado\n");
+    printf("r - Retangulo\n");
+    printf("t - Triangulo\n");
+    printf("z - Trapezio\n");
 
     scanf("%c", &opcao);
 
-    switch (opcao){
+    switch(opcao){
 
         case 'c':
-            printf("c - circulo\n");
+            printf("Circulo\n");
+            printf("Digite o valor do raio (r): ");
+            scanf("%f", &r);
+            A = M_PI * powf(r, 2);
             break;
-        
         case 'l':
-            printf("l - losango\n");
+            printf("Losango\n");
+            printf("Digite o valor da diagonal maior (D): ");
+            scanf("%f", &D);
+            printf("Digite o valor da diagonal menor (d): ");
+            scanf("%f", &d);
+            A = (D * d)/2;
             break;
-
         case 'q':
-            printf("q - quadrado\n");
+            printf("Quadrado\n");
+            printf("Digite o valor do lado (L): ");
+            scanf("%f", &L);
+            A = L * L;
             break;
-        
         case 'r':
-            printf("r - retangulo\n");
+            printf("Retangulo\n");
+            printf("Digite o valor da base (b): ");
+            scanf("%f", &b);
+            printf("Digite o valor da altura (h): ");
+            scanf("%f", &h);
+            A = b * h;
             break;
-
         case 't':
-            printf("t - triangulo\n");
+            printf("Triangulo\n");
+            printf("Digite o valor da base (b): ");
+            scanf("%f", &b);
+            printf("Digite o valor da altura (h): ");
+            scanf("%f", &h);
+            A = (b * h)/2;
             break;
-        
         case 'z':
-            printf("z - trapezio\n");
+            printf("Trapezio\n");
+            printf("Digite o valor da base maior (B): ");
+            scanf("%f", &B);
+            printf("Digite o valor da base menor (b): ");
+            scanf("%f", &b);
+            printf("Digite o valor da altura (h): ");
+            scanf("%f", &h);
+            A = ((B + b)*h)/2;
             break;
-
         default:
-            printf("Opçao invalida");
+            printf("Opcao invalida\n");
             A = 0;
             break;
-        
     }
-    
+    printf("A = %.2f\n", A);
     return 0;
 }
--- atividade4/Assistente_Virtual.c
+++ atividade4/Assistente_Virtual.c
@@ -1,31 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]){
 
-    char opcao_a;
-    float L,I ; 
-
-        printf("escolha o tipo de filme\n");
-        printf("L - Leve\n");
-        printf("I - Intenso\n");
-
-        scanf("%f", &opcao_a);
+    printf("Ola usario!\n\n");
     
-        switch(opcao_a);
-            Case 'L':
-            char opcao_a1;
-            float C,A;
-            printf("C - comedia\n");
-            printf("A - animacao\n");
-            scanf("%f", &C,A);
-            
-      
-            if(C){
-            printf("filme recomendado: As branquelas\n");
+    //1 
+    int leveintenso;
+        printf("Qual de tipo de filme deseja assistir:\n1 - Leve\n2 - Intenso\n\n");
+        scanf("%d", &leveintenso);
+
+    //2.1 
+        if (leveintenso == 1){
+        
+            int comediaanimacao;
+            printf("Esolha o tipo de filme leve que desaja assistir\n1 - Comedia\n2 - Animacao\n\n");
+            scanf("%d", &comediaanimacao);
+        //3.1
+            if(comediaanimacao == 1){     
+                printf("Assistir filme 'As branquela'\n\n\n");
             }
-            if(A){
-            printf("filme recomendado: Toy Story\n");
+        //3.2
+            if(comediaanimacao == 2){    
+                printf("Assistir filme 'Toy Story'\n\n\n");
             }
-        break;
-    return 0;
+    }
+    //paso 2.2
+        if(leveintenso == 2){
+            int terroracao;
+            printf("Esolha o tipo de filme intenso que desaja assistir\n1 - Terror\n2 - Acao\n\n");
+            scanf("%d", &terroracao);
+            //3.3
+            if(terroracao == 1){
+                int psicologicosobreantural;
+                printf("Esolha o tipo de filme terror que desaja assistir\n1 - Terror psicologico\n 2 - Terror sobrenatural\n\n");
+                scanf("%d", &psicologicosobreantural);  
+                //4.1
+                if(psicologicosobreantural == 1){
+                    printf("Assistir filme 'Corra!'\n\n\n");
+                }
+                //4.2
+                if(psicologicosobreantural == 2){
+                    printf("Assistir filme 'Invocacao do mal'\n\n\n");
+                }
+            }    
+            //3.4
+            if(terroracao == 2){
+                int super_heroisaventura_realista;
+                printf("Esolha o tipo de filme terror que desaja assistir\n1 - Super-Herois\n2 - Aventura realista\n\n");
+                scanf("%d", &super_heroisaventura_realista);
+                //4.3
+                if(super_heroisaventura_realista == 1){
+                    printf("Assistir filme 'Vingadores:Ultimato'\n\n\n");
+                }
+                //4.4
+                if(super_heroisaventura_realista == 2){
+                    printf("Assistir filme 'Mad Max: Estrada Furia'\n\n\n");
+                }
+                
+            }
+        }
+    
+        return 0;
 }
