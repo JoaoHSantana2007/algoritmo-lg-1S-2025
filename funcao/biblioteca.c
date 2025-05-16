e66428238b206eb5bb2620a4c3264422bd8a89c2
JoaoHSantana2007 <jhfcsant2007@gmail.com>
2025-05-16 12:30:13 -0300

    acriando bibliotecas

--- funcao/biblioteca.c
+++ funcao/biblioteca.c
@@ -1,6 +1,7 @@
-/*
-    retorna o mairo entre dois numeros inteiros.
-*/
+
+// Maior
+//retorna o mairo entre dois numeros inteiros.
+
 int  maior_2n(int a, int b){
 
     if(a > b){
@@ -11,11 +12,30 @@ int  maior_2n(int a, int b){
     }
 }
 
+
+//Retorna o maior entre tres numeors inteiros
+int  maior_3n(int a, int b, int c){
+    return maior_2n(maior_2n(a,b),c);
+}
+
+//Menor
+//retorna o menor entre dois numeros inteiros.
+
+int  menor_2n(int a, int b){
+
+    if(a < b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+
 /*
     Retorna o maior entre tres numeors inteiros
 */
-int  maior_3n(int a, int b, int c){
-    return maior_2n(maior_2n(a,b),c);
+int  menor_3n(int a, int b, int c){
+    return menor_2n(menor_2n(a,b),c);
 }
 
 
