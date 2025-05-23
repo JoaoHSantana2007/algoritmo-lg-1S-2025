bdf81ab64325b6ab70b446e92e416d1ec30a9113
JoaoHSantana2007 <jhfcsant2007@gmail.com>
2025-05-23 14:16:09 -0300

    add notas alunos

--- funcao/Notas_5_Alunos.c
+++ funcao/Notas_5_Alunos.c
@@ -1,48 +1,51 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
+
 #define TAM_NOME 64
 
 typedef struct Aluno{
     char nome[TAM_NOME];
     float nota;
-}Aluno;
+} Aluno;
 
 void imprimir_aluno(Aluno a){
-    printf("Nome: %sNome: %.2f\n\n", a.nome, a.nota);
-}
+    printf("Nota: %.2f - %s\n", a.nota, a.nome);
+} 
 
 int main(int argc, char* argv[]){
-    int n = 5;
-    float soma=0, media, max=0.0, min=10.0;
-    char maxAL, minAL;
+
+    int n = 3;
     Aluno alunos[n];
 
-    for(int i=1; i <= n; i++){
-        printf("\nDigite o nome do aluno %d: ", i);
-        fgets(alunos[i].nome, TAM_NOME, stdin); 
+    for(int i = 0; i < n; i++){
+        printf("Digite o nome do aluno %d: \n", i);
+        fgets(alunos[i].nome, TAM_NOME, stdin);
         printf("Digite a nota do aluno %d: ", i);
         scanf("%f", &alunos[i].nota);
-        getchar();
-    }    
-    for(int i=1; i<= n; i++){  
-                if(alunos[i].nota > max){
-                    max = alunos[i].nota;
-                    maxAL = i;
-                }
-                if(alunos[i].nota < min){
-                    min = alunos[i].nota;
-                    minAL = i;
-                }
-                soma =+ alunos[i].nota;
-                media = soma / n;
-            }
+        fflush(stdin);
+    }
 
-    printf("\nO aluno com a maior nota eh: %s", alunos[maxAL].nome);
-    printf("A sua nota foi: %2.f\n", max);
-    printf("\nO aluno com a menor nota eh: %s", alunos[minAL].nome);
-    printf("A sua nota foi: %2.f\n", min);
-    printf("\nA media dos alunos eh: %.2f\n", media);
+    Aluno aluno_maior_nota = alunos[0];
+    Aluno aluno_menor_nota = alunos[0];
+    float soma_notas = 0.0;
+    float media_notas = 0.0;
+    
+    for(int i = 0; i < n; i++){
+        if(aluno_maior_nota.nota < alunos[i].nota){
+            aluno_maior_nota = alunos[i];
+        }
 
-    return 0;   
+        if(aluno_menor_nota.nota > alunos[i].nota){
+            aluno_menor_nota = alunos[i];
+        }
+
+        soma_notas += alunos[i].nota;
+    }
+
+    printf("Aluno maior nota\n");
+    imprimir_aluno(aluno_maior_nota);
+    printf("Aluno menor nota\n");
+    imprimir_aluno(aluno_menor_nota);
+    printf("Media das notas: %.2f", soma_notas/n);
+
+    return 0;
 }
