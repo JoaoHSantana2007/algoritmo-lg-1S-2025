df1cd3afb92befe9339d5e284f26296e6f94adc0
JoaoHSantana2007 <jhfcsant2007@gmail.com>
2025-05-23 10:52:33 -0300

    add atividade alunos,notas;

--- funcao/teste.c
+++ funcao/teste.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 #define TAM_NOME 64
 
 typedef struct Aluno{
@@ -13,36 +13,39 @@ void imprimir_aluno(Aluno a){
 
 int main(int argc, char* argv[]){
     int n = 5;
-    float soma, media, max=0, min=10;
+    float soma, max=0.0, min=10.0;
+    int maxAL, minAL;
     Aluno alunos[n];
 
     for(int i=1; i <= n; i++){
         do{
-            printf("Digite o nome do aluno %d: ", i);
+            printf("\nDigite o nome do aluno %d: ", i);
             fgets(alunos[i].nome, TAM_NOME, stdin); 
             printf("Digite a nota do aluno %d: ", i);
             scanf("%f", &alunos[i].nota);
             getchar();
             if(alunos[i].nota < 0 || alunos[i].nota > 10){
-                printf("Erro\nDigite uma nota valida entre 0 e 10");
+                printf("\nErro\nDigite uma nota valida entre 0 e 10");
             }else{
                 if(alunos[i].nota > max){
                     max = alunos[i].nota;
+                    maxAL = i;
                 }
                 if(alunos[i].nota < min){
                     min = alunos[i].nota;
+                    minAL = i;
                 }
                 soma =+ alunos[i].nota;
             }
         }while(alunos[i].nota < 0 || alunos[i].nota > 10); 
     }   
-    media = soma / n;
+    float media = soma / n;
 
-    printf("A maior nota eh: %s\n", max);
-    printf("A menor nota eh: %s\n", min);
-    printf("A media nota eh: %s\n", media);
+    printf("\nO aluno com a maior nota eh: %s", alunos[maxAL].nome);
+    printf("A sua nota foi: %2.f\n", max);
+    printf("\nO aluno com a menor nota eh: %s", alunos[minAL].nome);
+    printf("A sua nota foi: %2.f\n", min);
+    printf("\nA media dos alunos eh: %.2f\n", media);
 
-    
-    
     return 0;   
 }
